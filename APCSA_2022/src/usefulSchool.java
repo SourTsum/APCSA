009004173c7b06f2819d5c3948ae29e86ec4822f
astellato <astellato@FLHSSTU112.fairfield.k12.ct.us>
2022-09-09 09:13:07 -0400

    Testing out the hashmap datastructure in Java, String to String.

--- APCSA_2022/src/usefulSchool.java
+++ APCSA_2022/src/usefulSchool.java
@@ -1,13 +1,13 @@
 
-import java.util.Scanner;
-
+import java.util.HashMap;
+import java.util.Map;
 public class usefulSchool {
 	public static void main(String[] args) {
 		
-		Scanner consoleIn = new Scanner(System.in);
+		Map<String, String> hm = new HashMap<String,String>();
+		hm.put("Testing","Passed");
 		
 		
-		
-		consoleIn.close();
+		System.out.println(hm.get("Testing"));
 	}
 }
