47267320ed13efca83e433f3fb723f527cd64def
astellato <astellato@FLHSSTU112.fairfield.k12.ct.us>
2022-09-09 09:05:35 -0400

    Imported Scanner to the userfulSchool class

--- APCSA_2022/src/usefulSchool.java
+++ APCSA_2022/src/usefulSchool.java
@@ -1,6 +1,13 @@
 
+import java.util.Scanner;
+
 public class usefulSchool {
 	public static void main(String[] args) {
 		
+		Scanner consoleIn = new Scanner(System.in);
+		
+		
+		
+		consoleIn.close();
 	}
 }
